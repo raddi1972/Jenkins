17e1c397dfe62b5eace2d0be21aad8d8bebc58d7
Rudransh <dixitrudransh01@gmail.com>
2022-11-25 01:54:17 +0530

    added stuff

--- test_fail.py
+++ test_fail.py
@@ -4,27 +4,20 @@ import unittest
 from leap_year import leap_year_tester
 
 class TestSum(unittest.TestCase):
-    def test1(self):
-        """
-        Test case to add two numbers
-        """
-        data = 1900
-        result = leap_year_tester(data)
-        self.assertEqual(result, False)
 
-    def test2(self):
+    def test4(self):
         """
         Test case to add two numbers
         """
-        data = 2020
+        data = 1800
         result = leap_year_tester(data)
         self.assertEqual(result, True)
-    
-    def test3(self):
+
+    def test5(self):
         """
         Test case to add two numbers
         """
-        data = 1900
+        data = 1600
         result = leap_year_tester(data)
         self.assertEqual(result, False)
 
